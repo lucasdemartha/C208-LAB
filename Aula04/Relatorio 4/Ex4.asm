3c1d27554567fb620ecb713a82d822d43442ffd3
lucasdemartha <lucasribm@gmail.com>
2023-09-26 09:46:17 -0300

    rel 4

---
+++ Aula04/Relatorio 4/Ex4.asm
@@ -0,0 +1,14 @@
+#Exercicio4
+
+.text
+
+li $t0, 324
+li $t1, 100
+li $t2, 2018
+li $t3, 1970
+li $t4, 33
+li $t5, 66
+
+nor $s0, $t0, $t1
+nor $s1, $t2, $t3
+nor $s2, $t5, $t4
---
+++ Aula04/Relatorio 4/Ex5.asm
@@ -0,0 +1,59 @@
+.text
+
+li $t0, 2 #a
+li $t1, 4 #b
+li $t2, 1 #c
+
+#1 a==b
+
+beq $t0, $t1, ELSE1
+	add $t2, $t0, $t1
+	sub $t0, $t1, $t2
+	j END1
+ELSE1:
+	add $t1, $t0, $t2
+	sub $t2, $t1, $t2
+END1:
+
+#2 a!=b
+
+bne $t0, $t1, ELSE2
+	add $t2, $t0, $t1
+	sub $t0, $t1, $t2
+	j END2
+ELSE2:
+	add $t1, $t0, $t2
+	sub $t2, $t1, $t2
+END2:
+
+#3 a>b
+bge $t0, $t1, ELSE3
+	add $t2, $t0, $t1
+	sub $t0, $t1, $t2
+	j END3
+ELSE3:
+	add $t1, $t0, $t2
+	sub $t2, $t1, $t2
+END3:
+
+#4 a<b
+ble $t0, $t1, ELSE4
+	add $t2, $t0, $t1
+	sub $t0, $t1, $t2
+	j END4
+ELSE4:
+	add $t1, $t0, $t2
+	sub $t2, $t1, $t2
+END4:
+
+#5
+li $t3, 10
+li $t4, 0
+
+WHILE:
+	beqz $t3, EXIT
+	addi $t4, $t4, 5
+	subi $t3, $t3, 1
+	j WHILE
+EXIT:
+
